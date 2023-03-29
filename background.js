00d32f412a6410ca37e4e165f770a4a1494a70fc
Loick Orville <orvllck.gmail.com>
2023-03-29 16:31:31 +0200

    wednesday 29.03

--- background.js
+++ background.js
@@ -1,80 +1,55 @@
-// Initialize the variables to store the matched record and the current tab's URL
-let matchedRecord = null;
-let currentTabUrl = '';
+// Your Airtable API key and base ID
+const AIRTABLE_API_KEY = "patBbYAg5Zg7PTxmi.b391bcc6f30a444bcc90041b79601c2fa5065ea4ff386fbb8f129351870e11bb";
+const AIRTABLE_BASE_ID = "appBUKPhtwQNDepbl";
+const AIRTABLE_TABLE_NAME = "perks";
 
-// Get the URL of the active tab in the current window
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  currentTabUrl = tabs[0].url;
-  // Now you can use the currentTabUrl variable in other functions or operations.
-});
-
-// Function to extract the domain name from a given URL
-function getDomain(currentTabUrl) {
-  const domain = new URL(currentTabUrl).hostname;
-  console.log(domain)
-  // Remove "www." if it's present in the domain name
+// Get the domain from the URL
+function getDomain(url) {
+  const domain = new URL(url).hostname;
   return domain.startsWith("www.") ? domain.substring(4) : domain;
 }
 
-// Function to check if a given domain name is present in the Airtable database
+// Check if the domain is present in the Airtable database
 async function checkAirtableForDomain(domain) {
-  // Replace with your Airtable API key and base ID
-  const apiKey = "patBbYAg5Zg7PTxmi.b391bcc6f30a444bcc90041b79601c2fa5065ea4ff386fbb8f129351870e11bb";
-  const baseId = "appBUKPhtwQNDepbl";
-  const tableName = "perks";
-
-  // Set up the request options for the Airtable API call
-  const requestOptions = {
-    method: "GET",
-    headers: {
-      "Authorization": `Bearer ${apiKey}`,
-      "Content-Type": "application/json",
-    },
-  };
-
-  // Build the API URL to query the Airtable database
-  const apiUrl = `https://api.airtable.com/v0/${baseId}/${tableName}?filterByFormula=({Domain}='${domain}')`;
-
-  // Fetch the data from the Airtable API and check if the domain is present
-  const response = await fetch(apiUrl, requestOptions);
-  if (!response.ok) {
-    throw new Error(`Error fetching data from Airtable: ${response.statusText}`);
-  }
+  const response = await fetch(
+    `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}?filterByFormula=({Domain}='${domain}')`,
+    {
+      headers: {
+        Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+      },
+    }
+  );
   const data = await response.json();
+  const records = data.records;
 
-  // If a matching record is found, store it in the matchedRecord variable and return true
-  if (data.records.length > 0) {
-    matchedRecord = data.records[0];
+  if (records.length > 0) {
+    matchedRecord = records[0];
     return true;
   } else {
-    // If no match is found, set matchedRecord to null and return false
-    matchedRecord = null;
     return false;
   }
 }
 
-// When a new tab is active, check if the domain matches a record in the Airtable database
-chrome.tabs.query({ active: true, currentWindow: true }, async function(tabs) {
-  currentTabUrl = tabs[0].url;
+// Listen for a click on the extension's icon
+chrome.action.onClicked.addListener(async (tab) => {
+  const currentTabUrl = tab.url;
   const currentDomain = getDomain(currentTabUrl);
   const matchFound = await checkAirtableForDomain(currentDomain);
 
-  // If a match is found, change the extension icon
   if (matchFound) {
-    chrome.browserAction.setIcon({ path: "match/icon-match-48.png", tabId: tabs[0].id });
+    chrome.extension.getBackgroundPage().matchedRecord = matchedRecord;
+  } else {
+    chrome.extension.getBackgroundPage().matchedRecord = null;
   }
 });
 
-// Listen for messages from the popup script and respond accordingly
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  // If the request is to get the matched record, return the matched record as the response
-  if (request.type === "getMatchedRecord") {
-    sendResponse({ record: matchedRecord });
-  } else if (request.type === "getRecommendedRecords") {
-    // If the request is to get recommended records, fetch the records and return them as the response
-    getRecommendedRecords().then(records => {
-      sendResponse({ records });
-    });
-    return true; // Indicate that the response will be sent asynchronously
+// Add a listener for messages from other parts of the extension
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === "getMatchedRecord") {
+    if (matchedRecord) {
+      sendResponse({ matchedRecord: matchedRecord });
+    } else {
+      sendResponse({ matchedRecord: null });
+    }
   }
 });
--- popup.js
+++ popup.js
@@ -1,50 +1,42 @@
-// Add a listener for when the document (popup window) has finished loading
-document.addEventListener("DOMContentLoaded", function () {
-    // Send a message to the background script requesting the matched record
-    chrome.runtime.sendMessage({ type: "getMatchedRecord" }, function (response) {
-      // If a matched record is found, display it; otherwise, display the "no match" message
-      if (response.record) {
-        displayMatchedRecord(response.record);
-      } else {
-        displayNoMatch();
-      }
-    });
+// Display the matched record's information
+function displayMatchedRecord(record) {
+  const container = document.getElementById("matched-record-container");
+  const recordDiv = document.getElementById("matched-record");
+
+  recordDiv.innerHTML = `
+    <h3>${record.fields.Name}</h3>
+    <p>${record.fields.Deal}</p>
+    <p>Value: ${record.fields.Value}</p>
+    <a href="${record.fields.Activation}" target="_blank">Activate Deal</a>
+  `;
+
+  container.classList.remove("hidden");
+}
+
+// Display the "no match" state
+function displayNoMatch() {
+  const container = document.getElementById("no-match-container");
+  container.classList.remove("hidden");
+
+  const proposePerkBtn = document.getElementById("propose-perk-btn");
+  proposePerkBtn.addEventListener("click", function () {
+    // Replace with the URL to the page where users can propose perks
+    const proposePerkUrl = "https://google.com";
+    window.open(proposePerkUrl);
   });
-  
-  // Function to display the matched record in the popup window
-  function displayMatchedRecord(record) {
-    // Get the HTML elements for the matched record container and the record itself
-    const container = document.getElementById("matched-record-container");
-    const recordDiv = document.getElementById("matched-record");
-  
-    // Fill the matched record element with the record's information (name, deal description, value, and link)
-    recordDiv.innerHTML = `
-      <h3>${record.fields.Name}</h3>
-      <p>${record.fields["Deal description"]}</p>
-      <p>Value: ${record.fields.Value}</p>
-      <a href="${record.fields["Link to activation"]}" target="_blank">Activate Deal</a>
-    `;
-  
-    // Make the matched record container visible
-    container.classList.remove("hidden");
-  }
-  
-  // Function to display the "no match" message in the popup window
-  function displayNoMatch() {
-    // Get the HTML element for the "no match" container
-    const container = document.getElementById("no-match-container");
-  
-    // Make the "no match" container visible
-    container.classList.remove("hidden");
-  
-    // Add a click event listener to the "Propose as perk" button
-    const proposePerkBtn = document.getElementById("propose-perk-btn");
-    proposePerkBtn.addEventListener("click", function () {
-      // Replace the URL below with the URL to the page where users can propose perks
-      const proposePerkUrl = "https://example.com/propose-perk";
-  
-      // Open the propose perk URL in a new window
-      window.open(proposePerkUrl);
-    });
-  }
-  
+}
+
+// Add an event listener for when the DOM (Document Object Model) is fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  // Send a message to the background script to request the matched record
+  chrome.runtime.sendMessage({ type: "getMatchedRecord" }, function (response) {
+    if (response.matchedRecord) {
+      // If there's a matched record, display its information
+      displayMatchedRecord(response.matchedRecord);
+    } else {
+      // If there's no matched record, display the "no match" state
+      displayNoMatch();
+    }
+  });
+});
+
